5598a56d7977885d087b20a38a09defea9b4af9a
srg <skabassjob@gmail.com>
2024-11-01 16:34:00 +0400

    feat: add scroll to top on change page

--- src/layout/PageLayout/PageLayout.tsx
+++ src/layout/PageLayout/PageLayout.tsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx'
 import styles from './PageLayout.module.scss'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 interface PageLayoutProps {
   children?: React.ReactNode
@@ -7,6 +9,13 @@ interface PageLayoutProps {
 }
 
 function PageLayout({ children, className }: PageLayoutProps): JSX.Element {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    const root = document.querySelector('#root')
+    root?.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }, [pathname])
+
   return <div className={clsx(styles.page, className)}>{children}</div>
 }
 
